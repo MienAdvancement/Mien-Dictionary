9db4acb7f6df38b8aac631fd4c62762c4675ae6d
MienAdvancement <phankal@comcast.net>
2026-01-08 20:15:10 -0800

    Fix missing material icons in web build

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3c958bfdaefc24e3a97243fdb16c1793
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "30a4045b50c988f0a0a099fb32755b5f",
+"flutter_bootstrap.js": "982f6303c1b7b18483f3a166b1475932",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
