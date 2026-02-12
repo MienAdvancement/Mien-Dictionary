a7b42030439e7e17888d777b3c28be0b2a24bbe2
MienAdvancement <phankal@comcast.net>
2026-02-12 03:36:38 -0800

    Deploy web build (feb12 15666 entries)

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "150089015"
+    serviceWorkerVersion: "704625354"
   }
 });
