86e0516399f3959bfa045c4d014dfbe9fcfd6f1a
MienAdvancement <phankal@comcast.net>
2026-02-13 23:45:42 -0800

    Deploy updated web build

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -2078,7 +2078,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "004ee39d3d43cf45afd8cbd7d5d5ceed
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "9218dac0f51142b36b6997ec779da802",
+"flutter_bootstrap.js": "92ff29deab0ca688705520b3afba67f2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
